3706224cc711ddf5cd31718f7eb063feaa4309be
Victor <victorstudios28@gmail.com>
2022-07-21 17:28:45 -0700

    added better pic, worked on the mobile to desktop transition

--- src/components/home-page/title.js
+++ src/components/home-page/title.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import '../../styles.css';
+import Logo from '../pics/logo.png';
+import TransparentLogo from '../pics/transparentLogo.png';
 
 export const Title = () => {
   return (
-    <Box className='title'>
-      <Box
-        sx={{
-          fontSize: '40px',
-          pt: 20,
-          color: 'red',
-          opacity: 1, 
-        }}
-      >
-        Phil's Construction
+    <Box>
+      <Box className='title'>
+        <Box
+          component='img'
+          sx={{
+            height: 200,
+            width: 200,
+            borderRadius: 5,
+            boxShadow: 10,
+            ml: 20,
+            '@media (max-width: 1000px)': {
+              ml: 0,
+              // alignItems: 'center',
+            },
+            '@media (max-width: 500px)': {
+              ml: 0,
+              height: 175,
+              width: 175,
+            },
+            
+          }}
+          src={Logo}
+        ></Box>
       </Box>
+      <Container sx={{mt: 10}}>Hello</Container>
     </Box>
   );
 };
