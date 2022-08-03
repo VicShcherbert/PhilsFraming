26a44d179aec86975f4fafd4df9729e420e50639
Victor <victorstudios28@gmail.com>
2022-08-02 23:54:50 -0700

    chore: added  more text to the about us, and also added pics

--- src/components/body/about-us.js
+++ src/components/body/about-us.js
@@ -1,8 +1,19 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import { Box } from '@mui/system';
+import React from 'react';
+import Stairs1 from '../pics/Stairs1.jpg';
 
 export const AboutUs = () => {
-    return (<Box className="leftBox">
-        <Box className="leftBoxText">About Us!</Box>
-    </Box>)
-}
+  return (
+    <Box className='leftBox'>
+      <Box className='leftBoxHeader'>About Us!</Box>
+      <Box className='leftBoxContent'>
+        We are a construction company located in Spokane Washington. For over
+        thirty years, KMP Construction has been operating in the Spokane
+        community with a business mindset. We are also a non-profit
+        organization, meaning that me take that money and use it to fund our
+        breakfasts.
+      </Box>
+      <Box component='img' className="" src={Stairs1} />
+    </Box>
+  );
+};
--- src/components/home-page/title.js
+++ src/components/home-page/title.js
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Box, Container } from '@mui/material';
 import '../../styles.css';
 import Logo from '../pics/KMP Logo.PNG';
-import TransparentLogo from '../pics/KMP Logo Clear.png';
+// import TransparentLogo from '../pics/KMP Logo Clear.png';
 import { TitleInfoBox } from './info-box';
 
 export const Title = ({width}) => {
-  
-
   return (
     <Box>
       <Box className='title'>
